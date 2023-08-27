ef1965c146e40132f850f6c095a76f4b5a4ddcbe
Fugazzii <sichinavailia@gmail.com>
2023-08-28 01:26:34 +0400

    Refactoring

--- src/blockchain/blockchain.interface.ts
+++ src/blockchain/blockchain.interface.ts
@@ -1,3 +1,16 @@
-export interface BlockchainInterface {
-    run(): Promise<void>;
+import { Server as WebSocketServer } from "ws";
+import { TransactionInterface } from "../transaction";
+import { BlockInterface } from "../block/block.interface";
+
+export interface Node {
+    nodeToBalance: Map<Node, number>;
+    transactionsHistory: Array<TransactionInterface>;
+    chain: Array<BlockInterface>;
+    addresses: Array<string>;
+
+    server: WebSocketServer;
+
+    start(): void;
+    addNode(addr: string): void;
+    addTransaction(tx: TransactionInterface): void;
 }
--- src/blockchain/blockchain.ts
+++ src/blockchain/blockchain.ts
@@ -1,97 +1,51 @@
-import { BlockInterface, Block } from "../block";
-import { TransactionInterface, Transaction } from "../transaction";
-import { EllipticCurve, Secp256k1 } from "../ec";
+import { IncomingMessage } from "http";
+import WebSocket, { Server as WebSocketServer } from "ws";
+import { TransactionInterface } from "../transaction";
+import { Node } from "./blockchain.interface";
+import { Block, BlockInterface } from "../block";
 
-export class Blockchain {
+export class Blockchain implements Node {
+    public nodeToBalance: Map<Node, number>;
+    public transactionsHistory: Array<TransactionInterface>;
+    public chain: Array<BlockInterface>;
+    public server: WebSocketServer;
+    public addresses: Array<string>;
 
-    private readonly addressToBalance: Map<string, number>;
-
-    private readonly chain: Array<BlockInterface>;
-    
-    private pendingTransactions: Array<TransactionInterface>;
-    private miningReward: number;
-    private difficulty: number;
-
-    public constructor(
-        private readonly ellipticCurve: EllipticCurve = new Secp256k1()
-    ) {
-        this.addressToBalance = new Map<string, number>();
+    public constructor() {
+        this.nodeToBalance = new Map<Node, number>();
+        this.transactionsHistory = new Array<TransactionInterface>();
         this.chain = new Array<BlockInterface>();
-        this.pendingTransactions = new Array<TransactionInterface>();
-        this.miningReward = 100;
-        this.difficulty = 2;
+        this.addresses = new Array<string>();
+
+        this.server = new WebSocketServer({ port: 3333 });
     }
 
-    public run() {
-        this.createGenesisBlock();
-    }
-
-    private createGenesisBlock() {
-        const rn = Date.now();
-        return new Block(rn, [], "0");
-    }
-
-    public addNode(addr: string) {
-        this.addressToBalance.set(addr, 0);
-    }
-
-    public minePendingTransactions(rewardAddr: string) {
-        const rewardTx = new Transaction(
-            "",
-            rewardAddr,
-            this.miningReward,
-            this.ellipticCurve
-        );
-        this.pendingTransactions.push(rewardTx);
-    
-        const block = new Block(Date.now(), this.pendingTransactions, this.latestBlock.getHash());
-        
-        block.mineBlock(this.difficulty);
-    
-        console.log('Block successfully mined!');
-        
-        this.chain.push(block);
-        this.pendingTransactions = [];
-    }
-
-    public addTransactions(transaction: TransactionInterface) {
-
-        if(transaction.getAmount() <= 0) throw new Error("Invalid amount");
-
-        const pendingTxsForWallet: Array<TransactionInterface> = this.pendingTransactions.filter(
-            (tx: TransactionInterface) => tx.getFromAddr() === transaction.getFromAddr()
-        );
-
-        const walletBalance = this.addressToBalance.get(transaction.getFromAddr()) as number;
-
-        if(pendingTxsForWallet.length > 0) {
-            const totalPending = pendingTxsForWallet
-                .map((tx: TransactionInterface) => tx.getAmount())
-                .reduce((prev: number, curr: number) => prev + curr);
-            
-            const total = transaction.getAmount() + totalPending;
-            
-            if(total > walletBalance) throw new Error("Insufficient balance");
-        }
-
-        this.pendingTransactions.push(transaction);
-        console.log("New transaction added");
-    }
-
-    private updateBalance() {
-        for(let tx of this.pendingTransactions) {
-            
-            const from = tx.getFromAddr();
-            const to = tx.getToAddr();
-            const amount = tx.getAmount();
-
-            const fromBalance = this.addressToBalance.get(from) as number;
-            const toBalance = this.addressToBalance.get(to) as number;
-
-            this.addressToBalance.set(from, fromBalance - amount);
-            this.addressToBalance.set(to, toBalance + amount);
+    public start() {
+        while(true) {
+            this.mine();
         }
     }
+    public addNode(addr: string): void {}
+    public addTransaction(tx: TransactionInterface): void {}
 
-    private get latestBlock() { return this.chain[this.chain.length - 1]; }
+    private mine() {
+        const next = new Block(Date.now(), []);
+        next.mineBlock(2);
+        this.chain.push(next);
+
+        for(let addr of this.addresses) {
+            const client = new WebSocket(addr);
+            client.emit("block_mined", next);
+        }
+    }
+ 
+    private listen() {
+        this.server.on("connection", (data: IncomingMessage) => {
+
+        });
+
+        // on block_mine
+        // on new_transaction
+        // on new_user
+    }
 }
